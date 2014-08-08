01482b0d1605ca5570433c5c99fae83b2e70fa8e
zshirsh <zibs.shirsh@gmail.com>
2014-08-08 21:58:18 +0530

    first

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// empty placeholder
+var name="Shirsh Zibbu"
+var score=123
+var multiplier=7;
+var doubledip=5;
+$("#nameSpan").text(name);
+$("#scoreSpan").text(score);
+$("#multiplierSpan").text(multiplier);
+$("#doubledipSpan").text(doubledip);
